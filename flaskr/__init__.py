77543d319515d7ac85a336ae287f0e9417059840
hogesuke <miyado@gmail.com>
2014-10-10 08:31:26 +0900

    ディレクトリ、ファイル構成を変更

---
+++ flaskr/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import mysql.connector
+
+app = Flask(__name__)
+app.config.from_object('flaskr.config')
+
+db_connector = mysql.connector.connect(
+	user     = app.config['DB_USER'],
+	password = app.config['DB_PASSWORD'],
+	host     = app.config['DB_HOST'],
+	database = app.config['DB_NAME'],
+	charset  = app.config['DB_CHARSET'])
---
+++ flaskr/config.py
@@ -0,0 +1,12 @@
+# app configuration
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+# mysql configuration
+DB_USER = 'testuser'
+DB_PASSWORD = 'password'
+DB_HOST = '127.0.0.1'
+DB_NAME = 'go_lang_test'
+DB_CHARSET = 'utf8'
