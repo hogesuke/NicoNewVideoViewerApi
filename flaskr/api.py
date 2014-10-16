efd6006f810fd55e6412b0c6a6d190eb0bb24890
hogesuke <miyado@gmail.com>
2014-10-17 08:22:29 +0900

    /my/contributorsのpostを追加（途中）

--- flaskr/api.py
+++ flaskr/api.py
@@ -34,6 +34,25 @@ def get_videos_list():
 
 	return json.dumps(row, default=default)
 
+@app.route('/my/contributors/', methods=['POST'])
+def post_my_contributor():
+	contributor_id = request.form['id']
+
+	# TODO OAuthを実装した後に修正する(ログインユーザのuser_idを使用するように)
+	contributor_idの存在チェック
+	sel_cursor = db_connector.cursor(dictionary = True)
+	sel_cursor.execute('select id from contributors where id = %s', [contributor_id])
+	# TODO 取得できなかった場合に処理を中断する実装
+	sel_cursor.fetchone()
+	sel_cursor.close()
+
+	ins_cursor = db_connector.cursor(buffered = True)
+	ins_cursor.execute('insert into users_contributors (user_id, contributor_id) values (%s, %s)', [1, contributor_id])
+
+	# TODO insertできなかった場合の処理
+	db_connector.commit()
+	ins_cursor.close()
+
 def default(obj):
 	import calendar, datetime
 
