54a1e66d505bf7e75d69adfdad0d34be89272855
hogesuke <miyado@gmail.com>
2014-11-02 23:33:08 +0900

    config.pyを.gitignoreに追加

--- flaskr/config.py
+++ flaskr/config.py
@@ -5,8 +5,11 @@ USERNAME = 'admin'
 PASSWORD = 'default'
 
 # mysql configuration
-DB_USER = 'testuser'
+DB_USER     = 'testuser'
 DB_PASSWORD = 'password'
-DB_HOST = '127.0.0.1'
-DB_NAME = 'go_lang_test'
-DB_CHARSET = 'utf8'
+DB_HOST     = '127.0.0.1'
+DB_NAME     = 'go_lang_test'
+DB_CHARSET  = 'utf8'
+
+CONSUMER_KEY    = 'v8QjvbOqWIBtkYGI75KNl2xf3'
+CONSUMER_SECRET = 'cstLqCelFnj0EncYj9Jup45r3POuCfel5vnIKQLzMXZTCrFxXC'
