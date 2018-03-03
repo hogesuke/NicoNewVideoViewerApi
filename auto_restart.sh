64b7795cd93eb89ed31eee52e1cb36719535ca4d
Tatsuya Miyado <miyado@gmail.com>
2018-03-03 13:28:32 +0900

    Update auto_restart.sh

--- auto_restart.sh
+++ auto_restart.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
 PID=`ps aux | grep uwsgi | grep -v "grep" | awk '{ print $2 }'`
 
 if [ "$PID" != "" ]; then
   kill -9 $PID
 fi
 
-cd /home/hogesuke/nicotune/NicoNewVideoViewerApi
+cd $SCRIPT_DIR
 
 uwsgi uwsgi.ini &
